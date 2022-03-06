9fe4631df4404ecfe183a47c7a44f6250499d884
zachloh <leongaun17@gmail.com>
2022-03-06 21:16:14 +1100

    Create header and sidebar sections

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create an array to store all book objects, myLibrary
+// Prompt user for input
+// Store user input as an object (use constructor function to create new instance)
+// Create a function that adds the object to the array
+// Display the books as a table
+// Add a functionality to remove each book from the library
+// Add a button to toggle read status
+
+const myLibrary = [];
+
